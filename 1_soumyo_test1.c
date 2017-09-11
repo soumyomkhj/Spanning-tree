21beb1ed08cf23d50467cf15ccf161196863646f
soumyomkhj <soumyo.rocks@outlook.com>
2017-09-11 07:19:30 +0530

    changes
    in input

--- 1_soumyo_test1.c
+++ 1_soumyo_test1.c
@@ -1,26 +1,49 @@
 #include <stdio.h>
 #include <limits.h>
-#define V 5
-int minKey(int key[], bool set_MST[]);
-int printMST(int parent[], int n, int graph[V][V]);
-void primeALGO(int graph[V][V]);
 
+int minKey(float key[], bool set_MST[]);
+int printMST(int parent[], int n, float graph[V][V]);
+void primeALGO(float graph[V][V]);
+void calc_w(int *graph,int num,float *weight);
+float SD(int* x1,int* x2);
 
 int main(void)
 {
-   int graph[V][V] = {{0, 2, 0, 6, 0},
-                      {2, 0, 3, 8, 5},
-                      {0, 3, 0, 0, 7},
-                      {6, 8, 0, 0, 9},
-                      {0, 5, 7, 9, 0},
-                     };
-    primeALGO(graph);
+   int i,V, graph[V][2];
+   printf("Enter number of points\n");
+   scanf("%d",&V );
+   for (i = 0; i < V; i++) {
+     printf("enter X , Y for %i: ",i+1);
+     scanf("%d %d",&graph[i][0],&graph[i][1]);
+   }
+   int edg;
+   printf("enter number of Edges\n");
+   scanf("%d",&edg);
+   float weight[V][V]={0};
+   calc_w(graph,edg,weight);
+    primeALGO(weight);
     return 0;
 }
-
-int minKey(int key[], bool set_MST[])
+float SD(int *x1,int *x2)
 {
-   int min = INT_MAX, min;
+  int x=x1[0]-x2[0];
+  int y=x1[1]-x2[1];
+  return sqrt(x*x+y*y);
+}
+
+void calc_w(int *graph,int num,float *weight)
+{
+  int i,val[2];
+  for ( i = 0; i < num; i++) {
+    printf("Enter the btween of Edge : %d\n",i+1 );
+    scanf("%d %d",&val[0],&val[1] );
+    weight[(val[0]-1),(val[1]-1)]=sqrt(graph[val[0]-1],graph[val[1]-1]);
+  }
+}
+
+int minKey(float key[], bool set_MST[])
+{
+   float max = 10000, min;
 
    for (int v = 0; v < V; v++)
      if (set_MST[v] == false && key[v] < min)
@@ -29,17 +52,17 @@ int minKey(int key[], bool set_MST[])
    return min;
 }
 
-int printMST(int parent[], int n, int graph[V][V])
+int printMST(int parent[], int n, float graph[V][V])
 {
    printf("Edge   Weight\n");
    for (int i = 1; i < V; i++)
       printf("%d - %d    %d \n", parent[i], i, graph[i][parent[i]]);
 }
 
-void primeALGO(int graph[V][V])
+void primeALGO(float graph[V][V])
 {
      int parent[V];
-     int key[V];
+     float key[V];
      bool set_MST[V];
 
      for (int i = 0; i < V; i++)
@@ -55,7 +78,7 @@ void primeALGO(int graph[V][V])
         set_MST[u] = true;
 
         for (int v = 0; v < V; v++)
-          if (graph[u][v] && set_MST[v] == false && graph[u][v] <  key[v])
+          if (graph[u][v]!=0 && set_MST[v] == false && graph[u][v] <  key[v])
              parent[v]  = u, key[v] = graph[u][v];
      }
 
